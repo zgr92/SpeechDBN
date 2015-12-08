6af9ee63ff8ecbcb171bf55af9d94d0eaefa47e6
Michał Koziarski <michalkoziarski@gmail.com>
2015-12-08 10:06:32 +0100

    modified testing script for speech

--- code/test_speech.py
+++ code/test_speech.py
@@ -1,14 +1,23 @@
 from GRBM_DBN import test_GRBM_DBN
 from load_data_speech import load_data
 
-N_FRAMES = 9
+N_FRAMES = 11
+LAYER_SIZE = 512
+ITERATIONS = 5
+N_LAYERS = [2, 3, 4]
 
 datasets = load_data(n_frames=N_FRAMES)
 
-test_GRBM_DBN(finetune_lr=0.1, pretraining_epochs=[225, 75],
-             pretrain_lr=[0.002, 0.02], k=1, weight_decay=0.0002,
-             momentum=0.9, batch_size=128, datasets=datasets,
-             hidden_layers_sizes=[1024, 1024, 1024],
-             n_ins=39*N_FRAMES, n_outs=120,
-             filename='../data/speech.pickle')
+for _ in range(ITERATIONS):
+    for n_layers in N_LAYERS:
+        test_score, val_score = test_GRBM_DBN(finetune_lr=0.1, pretraining_epochs=[225, 75],
+            pretrain_lr=[0.002, 0.02], k=1, weight_decay=0.0002,
+            momentum=0.9, batch_size=128, datasets=datasets,
+            hidden_layers_sizes=[1024, 1024, 1024],
+            n_ins=39*N_FRAMES, n_outs=120,
+            filename='../data/speech.pickle')
+
+        log = '../data/speech.log'
+        with open(log, 'a') as f:
+            f.write('N_FRAMES=%d, LAYER_SIZE=%d, n_layers=%d, test_score=%f%%, val_score=%f%%\n' % (N_FRAMES, LAYER_SIZE, n_layers, test_score, val_score) 
 
