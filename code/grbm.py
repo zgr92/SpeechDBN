b4ed1fac5eab48483ca80de9d7fa6b2efb8482f4
Michał Koziarski <michalkoziarski@gmail.com>
2015-12-29 22:38:19 +0100

    lighter GRBM

--- code/grbm.py
+++ code/grbm.py
@@ -4,7 +4,6 @@ import theano
 import theano.tensor as T
 
 class GBRBM(RBM):
-
     def __init__(self, input, n_visible=784, n_hidden=500, \
                  W=None, hbias=None, vbias=None, numpy_rng=None, transpose=False,
                  theano_rng=None, weight_decay=0.0002):
@@ -12,12 +11,6 @@ class GBRBM(RBM):
                          W=W, hbias=hbias, vbias=vbias, numpy_rng=numpy_rng,
                          theano_rng=theano_rng, weight_decay)
 
-
-    def type(self):
-        return 'gauss-bernoulli'
-
-
-    # overwrite free energy function (here only vbias term is different)
     def free_energy(self, v_sample):            
         wx_b = T.dot(v_sample, self.W) + self.hbias
         vbias_term = 0.5 * T.dot((v_sample - self.vbias), (v_sample - self.vbias).T)
@@ -26,25 +19,7 @@ class GBRBM(RBM):
 
 
     def sample_v_given_h(self, h0_sample):
-
-        pre_sigmoid_v1, v1_mean = self.propdown(h0_sample)
-
-        '''
-            Since the input data is normalized to unit variance and zero mean, we do not have to sample
-            from a normal distribution and pass the pre_sigmoid instead. If this is not the case, we have to sample the
-            distribution.
-        '''
-        # in fact, you don't need to sample from normal distribution here and just use pre_sigmoid activation instead        
+        pre_sigmoid_v1, v1_mean = self.propdown(h0_sample)    
         v1_sample = self.theano_rng.normal(size=v1_mean.shape, avg=v1_mean, std=1.0, dtype=theano.config.floatX) + pre_sigmoid_v1
-        # v1_sample = pre_sigmoid_v1
         return [pre_sigmoid_v1, v1_mean, v1_sample]
 
-
-    def get_reconstruction_cost(self, updates, pre_sigmoid_nv):
-        """
-        RMS as the reconstructed cost
-
-        """
-
-        rms_cost = T.mean(T.sum((self.input -  pre_sigmoid_nv)** 2, axis=1))
-        return rms_cost
